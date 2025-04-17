b633b0cbd69c5359c580599d32f4b59e3d29bba5
Felix Liwonde <bed-com-21-20@unima.ac.mw>
2025-04-17 05:37:34 +0200

    face upload fucntionality changed

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,21 +1,34 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from '../Entities/users.entity';
+import * as faceapi from '@vladmandic/face-api';
+import * as canvas from 'canvas';
 
-@Injectable()
+const { Canvas, Image, ImageData } = canvas;
+
+faceapi.env.monkeyPatch({
+  Canvas: Canvas as unknown as typeof HTMLCanvasElement,
+  Image: Image as unknown as typeof HTMLImageElement,
+  ImageData: ImageData as unknown as typeof globalThis.ImageData,
+});
+
+
 export class UsersService {
-  constructor(
-    @InjectRepository(User)
-    private readonly userRepo: Repository<User>,
-  ) {}
+  async registerUser(name: string, imagePath: string) {
+    const img = await canvas.loadImage(imagePath);
 
-  async registerUser(name: string, faceEmbedding: Buffer): Promise<User> {
-    const user = this.userRepo.create({ name, faceEmbedding });
-    return this.userRepo.save(user);
-  }
+    const detection = await faceapi
+      .detectSingleFace(img as unknown as faceapi.TNetInput)
+      .withFaceLandmarks()
+      .withFaceDescriptor();
 
-  async findAll(): Promise<User[]> {
-    return this.userRepo.find();
+    if (!detection) {
+      throw new Error('No face detected!');
+    }
+
+    const faceEmbedding = detection.descriptor; // this is a Float32Array
+
+    // Store user with name + embedding in database
+    return {
+      name,
+      faceEmbedding: Array.from(faceEmbedding), // Save it as JSON or Buffer
+    };
   }
 }
