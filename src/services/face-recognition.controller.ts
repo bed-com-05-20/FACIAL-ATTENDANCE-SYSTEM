6ca50416a8ddefd5541f0aee18e43108e970ad27
Felix Liwonde <bed-com-21-20@unima.ac.mw>
2025-04-17 20:16:08 +0200

    nothing more

--- src/services/face-recognition.controller.ts
+++ src/services/face-recognition.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Post, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FaceRecognitionService } from '../services/face-recognition.services';
-import { Multer } from 'multer';
+import { diskStorage, Multer } from 'multer';
+//import { diskStorage } from 'multer';
+import { ApiConsumes, ApiBody, ApiTags } from '@nestjs/swagger';
 
 
 @Controller('face-recognition')
@@ -9,15 +11,39 @@ export class FaceRecognitionController {
   constructor(private readonly faceRecognitionService: FaceRecognitionService) {}
 
   @Post('detect')
-  @UseInterceptors(FileInterceptor('file'))
-  async detect(@UploadedFile() file: Multer.File)
+  @UseInterceptors(FileInterceptor('photo', {
+    storage: diskStorage({
+      destination: './uploads', 
+      filename: (req, file, cb) => {
+        const randomName = Array(4).fill(0).map(i => Math.random().toString
+        (36).charAt(2)).join('');
+      },
+ 
+    }),
+  
+  }))
+  //ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: { type: 'string' },
+        photo: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+
+  async detect(@UploadedFile() photo: Express.Multer.File)
   { 
-    if (!file) {
+    if (!photo) {
       throw new BadRequestException('No file uploaded');
     }
 
     // Pass the file buffer instead of path
-    const detections = await this.faceRecognitionService.detectFace(file.buffer);
+    const detections = await this.faceRecognitionService.detectFace(photo.buffer);
     
     return { detections };
   }
