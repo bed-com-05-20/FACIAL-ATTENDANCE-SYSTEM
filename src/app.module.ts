3caa7eaeab6260f6b6ba040b05027ef14381276d
Felix Liwonde <bed-com-21-20@unima.ac.mw>
2025-05-03 14:25:52 +0200

    minor changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './Entities/users.entity';
-import { FaceRecognitionController } from './services/face-recognition.controller';
-import { FaceRecognitionService } from './services/face-recognition.services';
 import { MulterModule } from '@nestjs/platform-express';
 import { FaceRecognitionModule } from './services/face-recognition.model';
+import { FaceEntity } from './Entities/face.entity';
 
 @Module({
   imports: [
@@ -21,8 +16,8 @@ import { FaceRecognitionModule } from './services/face-recognition.model';
       username: 'postgres', 
       password: 'admin', 
       database: 'mydb', 
-      entities: [User],
-      synchronize: false, 
+      entities: [User, FaceEntity],
+      synchronize: true, 
     }),
     UsersModule,
     FaceRecognitionModule,
