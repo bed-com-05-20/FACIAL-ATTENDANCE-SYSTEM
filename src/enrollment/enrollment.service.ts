37f8ac863054a0761d2e19475b852a30def047fe
bed-com-21-20 <bed-com-21-20@unima.ac.mw>
2025-06-08 17:39:29 +0200

    removed

--- src/enrollment/enrollment.service.ts
+++ src/enrollment/enrollment.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { EnrollmentDto } from 'src/dto/enrollment.dto';
 import { EnrollmentEntity } from 'src/entity/enrollment.entity';
-import { AttendanceEntity } from 'src/entity/attendance.entity';
+// import { AttendanceEntity } from 'src/entity/attendance.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -11,22 +11,22 @@ export class EnrollmentService {
     @InjectRepository(EnrollmentEntity)
     private enrollmentRepository: Repository<EnrollmentEntity>,
 
-    @InjectRepository(AttendanceEntity)
-    private attendanceRepository: Repository<AttendanceEntity>,
+    // @InjectRepository(AttendanceEntity)
+    // private attendanceRepository: Repository<AttendanceEntity>,
   ) {}
 
-  async enrollStudent(createStudentDto: EnrollmentDto): Promise<EnrollmentEntity> {
-    const student = this.enrollmentRepository.create(createStudentDto);
-    const savedStudent = await this.enrollmentRepository.save(student); 
+  // async enrollStudent(createStudentDto: EnrollmentDto): Promise<EnrollmentEntity> {
+  //   const student = this.enrollmentRepository.create(createStudentDto);
+  //   const savedStudent = await this.enrollmentRepository.save(student); 
 
    
-    await this.attendanceRepository.save({  
-      regNumber: savedStudent.regNumber,
-      status: 'Absent', 
-    });
+  //   await this.attendanceRepository.save({  
+  //     regNumber: savedStudent.regNumber,
+  //     status: 'Absent', 
+  //   });
 
-    return savedStudent; 
-  }
+  //   return savedStudent; 
+  // }
 
   async getStudentByRegNumber(regNumber: string): Promise<EnrollmentEntity> {
     const student = await this.enrollmentRepository.findOne({ where: { regNumber } });
