6a70c9f23059a5bceee3d9e1d54a05750595546c
bed-com-21-20 <bed-com-21-20@unima.ac.mw>
2025-06-08 10:39:50 +0200

    examssession added

--- src/services/face-recognition.model.ts
+++ src/services/face-recognition.model.ts
@@ -6,11 +6,16 @@ import { FaceEntity } from 'src/Entities/face.entity';
 import { AttendanceModule } from 'src/attendance/attendance.module';
 import { CameraModule } from 'src/camera/camera.module';
 import { GatewayModule } from 'src/face-gateway/face-gateway.module';
+import { ExamSessionEntity } from 'src/entity/examsession.entity';
+import { AttendanceHistoryEntity } from 'src/entity/history.entity';
 
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([FaceEntity]),
+    TypeOrmModule.forFeature([
+      FaceEntity,
+      ExamSessionEntity,
+      AttendanceHistoryEntity]),
     AttendanceModule,
      CameraModule,
     GatewayModule
