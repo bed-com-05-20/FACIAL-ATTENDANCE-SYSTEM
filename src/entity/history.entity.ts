0e092a8853f25f4fe054cb7b80eb11228cbf3013
bed-com-21-20 <bed-com-21-20@unima.ac.mw>
2025-06-08 17:39:56 +0200

    entity refactored

--- src/entity/history.entity.ts
+++ src/entity/history.entity.ts
@@ -1,25 +1,23 @@
 // attendance-history.entity.ts
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, ManyToOne, Column, CreateDateColumn } from 'typeorm';
 import { Students } from 'src/attendance/students.entity';
 import { ExamSessionEntity } from './examsession.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('attendance_history')
 export class AttendanceHistoryEntity {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   id: number;
 
-  @ManyToOne(() => Students, student => student.id)
+  @ManyToOne(() => Students, { eager: true })
   student: Students;
 
-  @ManyToOne(() => ExamSessionEntity, session => session.attendanceHistories)
+  @ManyToOne(() => ExamSessionEntity, { eager: true })
   examSession: ExamSessionEntity;
 
-  @Column({ 
-    type: 'enum', 
-    enum: ['Present', 'Absent'], 
-    default: 'Present' 
-  })
-  status: 'Present' | 'Absent';
+  @Column({ default: 'Present' })
+  status: string;
 
   @CreateDateColumn()
   markedAt: Date;
