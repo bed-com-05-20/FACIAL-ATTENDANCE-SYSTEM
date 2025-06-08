8780a23604e8aed869aff7eeacb7622b8ce5cd71
bed-com-21-20 <bed-com-21-20@unima.ac.mw>
2025-06-08 17:38:51 +0200

    attendance history controller added

--- src/attendance/attendance.module.ts
+++ src/attendance/attendance.module.ts
@@ -3,14 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Students} from './students.entity';
 import { AttendanceService } from './attendance.service';
 import { AttendanceController } from './attendance.controller';
-import { EnrollmentEntity } from 'src/entity/enrollment.entity';
-import { EnrollmentModule } from '../enrollment/enrollment.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Students])],
   controllers: [AttendanceController],
   providers: [AttendanceService],
-  exports:[AttendanceService, TypeOrmModule] // exports: [TypeOrmModule], // ✅ export TypeOrmModule so other modules can use Students
-
+  exports:[AttendanceService, TypeOrmModule] 
 })
 export class AttendanceModule {}
