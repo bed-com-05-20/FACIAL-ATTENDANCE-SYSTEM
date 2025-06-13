f2bee70cc15e663b9d343fd84f8cebbe0d4f6491
bed-com-21-20 <127156083+bed-com-21-20@users.noreply.github.com>
2025-06-13 08:35:39 +0200

    Update camera.service.ts

--- src/camera/camera.service.ts
+++ src/camera/camera.service.ts
@@ -8,7 +8,7 @@ export class CameraService {
   private readonly logger = new Logger(CameraService.name);
 
   // Base URL of your Flask server (no endpoint or query here)
-  private readonly piBaseUrl = 'https://fb49-102-70-93-108.ngrok-free.app';
+  private readonly piBaseUrl = 'https://1c56-137-115-6-85.ngrok-free.app';
 
   
     //Capture images from the Flask Pi server.
